76274c7df7897b17c2b7747ce8c68c64466ca4a3
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-01-24 14:52:20 -0500

    Add layer support for movie
    The movie holds a list of layers now

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,6 +1,45 @@
-import pytest
+import pytest   # noqa F401
 
 from vidar.movie import Movie
+from vidar.layer import Layer
+
 
 class TestMovie:
-    pass
+    def test_add_layer_attaches_layer(self, mocker):
+        movie = Movie(1, 1)
+        layer = Layer(1.0)
+        spy = mocker.spy(layer, 'attach')
+
+        movie.add_layer(0.0, layer)
+
+        spy.assert_called_once_with(movie)
+
+    def test_tracks_append_attaches_layer(self, mocker):
+        movie = Movie(1, 1)
+        layer = Layer(1.0)
+        spy = mocker.spy(layer, 'attach')
+
+        movie.tracks.append((0.0, layer))
+
+        spy.assert_called_once_with(movie)
+
+    def test_remove_layer_detaches_layer(self, mocker):
+        movie = Movie(1, 1)
+        layer = Layer(1.0)
+        movie.add_layer(0.0, layer)
+        spy = mocker.spy(layer, 'detach')
+
+        movie.remove_layer(layer)
+
+        spy.assert_called_once_with()
+
+    def test_tracks_filter_detaches_layer(self, mocker):
+        movie = Movie(1, 1)
+        test_layer = Layer(1.0)
+        movie.add_layer(0.0, test_layer)
+        spy = mocker.spy(test_layer, 'detach')
+
+        movie.tracks = [(time, layer) for time, layer in movie.tracks
+            if layer != test_layer]
+
+        spy.assert_called_once_with()
--- vidar/movie.py
+++ vidar/movie.py
@@ -9,5 +9,41 @@ class Movie:
         self.height = height
         self.background = background
 
+        self._tracks = Movie.Tracks(self)
         self._window = pyglet.window.Window(
             width=width, height=height, visible=False)
+
+    @property
+    def tracks(self):
+        return self._tracks
+
+    def add_layer(self, time, layer):
+        self.tracks.append((time, layer))
+        return self
+
+    def remove_layer(self, layer):
+        self.tracks = [(time, layer_) for time, layer_ in self.tracks
+            if layer_ != layer]
+
+    @tracks.setter
+    def tracks(self, value):
+        for time, layer in self.tracks:
+            layer.detach()
+
+        self._tracks = Movie.Tracks(self, value)
+        for time, layer in value:
+            layer.attach(self)
+
+    class Tracks(list):
+        def __init__(self, movie, iterable=()):
+            list.__init__(self, iterable)
+            self.movie = movie
+
+        def __delitem__(self, track):
+            track.detach()
+            list.__delitem__(self, track)
+
+        def append(self, track):
+            list.append(self, track)
+            time, layer = track
+            layer.attach(self.movie)
