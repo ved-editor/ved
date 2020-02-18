9648368714729e0e88ef12539bb2c792ba691224
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-02-18 13:02:32 -0500

    Make MediaLayer inherit Audiolayer

--- vidar/media/layers.py
+++ vidar/media/layers.py
@@ -1,16 +1,14 @@
-from io import BytesIO
 import struct
-import wave
 
 import pyglet
 
-from ..layer import Layer
+from ..audio import AudioLayer
 
 # Arbitrary: number of bytes to request at a time.
 _BUFFER_SIZE = 1 << 20  # 1 MB
 
 
-class MediaLayer(Layer):
+class MediaLayer(AudioLayer):
     def __init__(self, file):
         super().__init__(file)
         self.media = pyglet.media.load(file)
@@ -58,37 +56,6 @@ class MediaLayer(Layer):
             raise ValueError('no more audio data left')
         self.buffer = packet.get_string_data()
         self.buffer_pos = 0
-
-    def get_audio_data(self) -> bytes:
-        """Return the bytes for a wave file containing the audio of the layer
-        """
-        if self.audio_format is None:
-            raise ValueError('no audio data available')
-        fmt = self.audio_format
-        data = BytesIO()
-        # Note that there are multiple samples per frame if there is more than
-        # one layer
-        sample_index = 0
-        while True:
-            # Calculate time based on sample index
-            frame = sample_index // fmt.channels
-            time = frame / fmt.sample_rate
-            # Allow audio manipulation:
-            sample = self.sample(time)
-            if sample is None:
-                break
-            # Now convert the manipulated sample back to bytes
-            data.write(struct.pack('<c' if fmt.sample_size == 8 else '<h',
-                sample))
-            sample_index += 1
-        # Convert from sample data to a wav file
-        f = BytesIO()
-        wav = wave.open(f, 'wb')
-        wav.setnchannels(fmt.channels)
-        wav.setsampwidth(fmt.sample_size // 8)
-        wav.setframerate(fmt.sample_rate)
-        wav.writeframes(data.getvalue())
-        return f.getvalue()
 
 
 class VideoFormat:
