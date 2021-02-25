8b12b9d42f4976d364b3a98e336da73e27bac7fe
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-02-25 00:42:20 -0500

    Unit test audio sample output of record

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,11 @@
 import io
 import subprocess
+import struct
 
 import pytest   # noqa F401
 import imageio
 import numpy as np
+import wave
 
 from ved.movie import Movie
 from ved.node import Node
@@ -69,23 +71,50 @@ class TestMovie:
                     assert np.array_equal(np.array([0, 0, 0]), pixel)
 
     def test_record_can_save_audio_data_to_stream(self, mocker):
-        node = Audio(0.0, 1.0, 1, 8, 44100)
+        node = Audio(0.0, 0.01, 1, 8, 44100)
+        node.samples = 0.5,  # constant sample output (won't be overwritten)
         # width needs to be divisible by 2 for ffmpeg
         movie = Movie(2, 2, [node])
-        mocked_call = mocker.patch.object(node, '__call__')
-        mocked_call.return_value = 0.5
         result = io.BytesIO()
 
-        movie.record('.mp4', frame_rate=1, sample_rate=11025,
-            file=result)
+        # .avi files can copy the streams from the (lossness) wav files that
+        # are created by ved, so no data is lost.
+        movie.record('.avi', frame_rate=1, sample_rate=11025,
+            file=result, ffmpeg_options=['-c:a copy'])
 
         result.seek(0)
-        p = subprocess.Popen('ffprobe pipe: -v error', shell=True,
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+        p = subprocess.Popen('ffmpeg -f avi -i pipe: -f wav pipe: -v error',
+            shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
             stderr=subprocess.PIPE)
         stdout, stderr = p.communicate(input=result.getvalue())
 
         if stderr:
-            # only errors are printed, so we know there was an error
+            # '-v error' is set, so we know there was an error if stderr is
+            # nonempty.
             raise AssertionError(stderr)
-        # If there are no ffmpeg errors, pass
+
+        f = io.BytesIO(stdout)
+        wav = wave.open(f, 'rb')
+        n = wav.getnframes()
+        w = wav.getsampwidth()
+
+        # There should only be 1 audio channel, but the sample width may not be
+        # 1 byte. For now, this is acceptable, but TODO: investigate this.
+        def unpack_to_float(frame):
+            # if the sample width is 2, interpret bytes as a short
+            # if the sample width is 1, interpret them as a character
+            format_ = '<h' if w == 2 else '<b'
+            i = int(struct.unpack(format_, frame)[0])  # unpack bytes to int
+            # Subtract one because [-1, 1] spans 2 units already.
+            exp = 8 * w - 1
+            return i / (2.0 ** exp)  # scale int to float
+
+        # Read actual frames
+        frames = wav.readframes(n)
+        samples = []
+        for i in range(0, len(frames), w):
+            samples.append(unpack_to_float(frames[i:i + w]))
+
+        # Comapre with expected frames
+        expected_n = int(0.01 * 11025 + 1)  # + 1 for last frame
+        assert samples == [0.5] * expected_n
